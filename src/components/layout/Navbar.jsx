5245040b423b7bf3ba691252cc9871e529d4af83
Pratham <catchthepratham@gmail.com>
2025-09-23 18:36:23 +0530

    file paths updated

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -6,12 +6,11 @@ export default function NavBar({ activeTab, setActiveTab }) {
     return (
         <div className="navbar">
             <div className="navbar-left">
-                <h1 className="title">Teachers / Downloads</h1>
+                <h1 className="title">Teacher_name</h1>
                 <Tabs activeTab={activeTab} setActiveTab={setActiveTab} />
             </div>
             <div className="navbar-right">
                 <Button className="primary">Sign in</Button>
-                <Button className="outline">Try Whimsical</Button>
             </div>
         </div>
     );
